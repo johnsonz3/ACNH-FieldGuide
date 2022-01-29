d5467ec3200c45c42ef8a1a808c344a2b44172b8
Johnson <johnson7267@gmail.com>
2022-01-28 23:15:28 -0500

    Added toggleable navbar

--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,12 @@
       <span class="font-semibold text-xl tracking-tight">Animal Crossing Field Guide</span>
     </div>
     <div class="block lg:hidden">
-      <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+      <button @click="isOpen = !isOpen" class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
         <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
       </button>
     </div>
     <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-      <div class="text-sm lg:flex-grow">
+      <div :class="isOpen ? 'block' : 'hidden' " class="text-sm lg:flex-grow">
         <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
           Docs
         </a>
@@ -35,6 +35,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      isOpen : false,
+    }
+  },
   name: 'IndexPage'
 }
 </script>
