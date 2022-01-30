b7684ad40fb6f82b0e8fe8b6d285c5b51397be33
Johnson <johnson7267@gmail.com>
2022-01-30 00:40:37 -0500

    Finished

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
     </div>
     <div class="block lg:hidden">
       <button @click="isOpen = !isOpen" class="flex items-center px-3 py-2 border rounded border-none text-teal-200 border-teal-400 hover:text-white hover:border-white">
-        <svg class="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
+        <svg class="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="https://www.w3.org/2000/svg"><title>Menu</title>
         <path v-if="!isOpen" d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
         <path v-if="isOpen" fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
         </svg>
@@ -131,11 +131,11 @@ export default {
   },
   async fetch() {
     this.bugs = await fetch(
-      'http://acnhapi.com/v1/bugs'
+      'https://acnhapi.com/v1/bugs'
     ).then(res => res.json());
 
     this.fishes = await fetch(
-      'http://acnhapi.com/v1/fish'
+      'https://acnhapi.com/v1/fish'
     ).then(res => res.json());
   },
   created() {
